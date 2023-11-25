6eb3cdb6991a259794d4e154a08795fd79d6000a
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-25 22:53:44 +0300

    feat:makes the page responsive

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -3,20 +3,20 @@ import { servicesList } from './ServicesList';
 function ServicesCard() {   
   return (
     <>
-    <div>
-        <h1 className='ml-60 text-2xl font-semibold text-center'>
-                <span className='border-b-2 pb-3 text-color-tertiary'> WHAT WE DO </span>
-        </h1>
-        <div className='mb-5'>
-            <h1 className='ml-60 text-6xl font-semibold text-center'>
+        <div>
+            <h1 className='ml-4 lg:ml-60 text-2xl font-semibold text-center mb-4 lg:mx-4 lg:mb-5'>
+                <span className='border-b-2 pb-3 text-color-tertiary font-primary'> WHAT WE DO </span>
+            </h1>
+            <div className='mb-5'>
+                <h1 className='ml-4 lg:ml-60 text-4xl md:text-5xl lg:text-6xl font-semibold text-center'>
                 <span className='border-b-2 pb-3 '> Your Favourite Parcel</span>
-            </h1>
-            <h1 className='ml-60 text-6xl font-semibold text-center'>
-                <span className='border-b-2 pb-3 '> Delivery Partner </span>
-            </h1>
+                </h1>
+                <h1 className='ml-4 lg:ml-60 text-4xl md:text-5xl lg:text-6xl font-semibold text-center'>
+                <span className='border-b-2 pb-3'> Delivery Partner </span>
+                </h1>
+            </div>
         </div>
-    </div>
-    <div className='px-2 bg-color-secondary' > 
+        <div className='px-2 bg-color-secondary' >
         <div className=' w-10/12 m-auto justify-between flex flex-row flex-wrap '>  
             {servicesList.map((services,idx)=>(
                 <div key={idx} className=" max-w-sm my-8 overflow-hidden shadow-lg p-2">
