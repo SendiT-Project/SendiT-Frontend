a52d7954945c52325e7c8b41c21d871034bb6013
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-25 14:46:46 +0300

    adds hover effect on the navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 const NavBar = () => {
     return ( 
-        <div className="navbar flex justify-between">
+        <div className="navbar flex justify-between items-center px-8 cursor-pointer">
         <div>
-          <h1 className="text-2xl font-semibold px-8">SendiT</h1>
+          <h1 className="text-2xl font-semibold">SendiT</h1>
         </div>
-          <div className="flex w-1/4 justify-evenly">
+          <div className="hidden lg:flex w-1/4 justify-evenly items-center">
           <ul className="flex justify-evenly w-5/6 mt-2">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Tracker</li>
-            <li>Contact Us</li>
+            <li className="nav-link hover:text-rose-500">Home</li>
+            <li className="nav-link hover:text-rose-500">About Us</li>
+            <li className="nav-link hover:text-rose-500">Tracker</li>
+            <li className="nav-link hover:text-rose-500">Contact Us</li>
           </ul>
-          <button className="bg-color-tertiary text-white px-5 py-2 rounded-3xl text-base">login</button>
+          <button className="bg-color-tertiary hover:bg-rose-500 text-white px-5 py-2 rounded-3xl text-base cursor">login</button>
         </div>
       </div>
      );
