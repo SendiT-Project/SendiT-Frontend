7052c6faa086c50718b9443bac32eb5031aaab33
Ochuodho <ochuodhoderrick@gmail.com>
2023-11-29 17:48:20 +0300

    feat: sign

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavBar from "./components/Navbar";
 import ServicesCard from "./components/ServiceCard";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
+    <Router>
     <div className="App bg-color-primary px-8 sm:px-12 md:px-20 lg:px-36 py-10">
-      <div className="bg-color-secondary p-8 mx-4">
-        <NavBar />
-        <Home />
-        <ServicesCard />
-        <Footer />
-      </div>
+     
+        <div className="bg-color-secondary p-8 mx-4">
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+          <NavBar />
+          < Home />
+          <ServicesCard />
+          <Footer />
+        </div>
+      
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/SignUp.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const validationSchema = Yup.object().shape({
+    username: Yup.string()
+        .required("Username is required")
+        .min(6, 'Username must be at least 6 characters')
+        .max(20, 'Username must not exceed 20 characters'),
+    email: Yup.string()
+        .required('Email is required')
+        .email('Email is invalid'),
+    password: Yup.string()
+        .required('Password is required')
+        .min(6, 'Password must be at least 6 characters')
+        .max(40, 'Password must not exceed 40 characters'),
+    confirmPassword: Yup.string()
+        .required('Confirm Password is required')
+        .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+    acceptTerms: Yup.bool().oneOf([true], 'Accept Term is required'),
+});
+
+const SignUp = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (data, { resetForm }) => {
+        axios.post('https://127.0.0.1:5000/signup', {
+            username: data.username,
+            email: data.email,
+            password: data.password
+        })
+        .then(function (response) {
+            console.log(response);
+            alert('New user added.');
+            resetForm();
+        })
+        .catch(function (error) {
+            console.error(error);
+            alert("An error occurred. Please try again.");
+        });
+    };
+
+    const handleLoginLinkClick = () => {
+        navigate("/login");
+    };
+
+    return (
+        <div className="container mx-auto p-20 flex items-center justify-center h-screen">
+            <div className="max-w-md bg-color-secondary rounded overflow-hidden shadow-lg">
+                <h1 className="text-2xl text-center py-4 font-bold text-black">Sign Up</h1>
+                <Formik
+                    initialValues={{
+                        username: '',
+                        email: '',
+                        password: '',
+                        confirmPassword: '',
+                        acceptTerms: false,
+                    }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {(formikProps) => (
+                        <Form className="p-6">
+                            <div className="mb-4">
+                                <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+                                    User Name
+                                </label>
+                                <Field name="username" type="text" className="form-input border border-gray-300 rounded" />
+                                <ErrorMessage name="username" component="div" className="text-red-500 text-xs mt-1" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                                    Email
+                                </label>
+                                <Field name="email" type="email" className="form-input border border-gray-300 rounded" />
+                                <ErrorMessage name="email" component="div" className="text-red-500 text-xs mt-1" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+                                    Password
+                                </label>
+                                <Field name="password" type="password" className="form-input border border-gray-300 rounded" />
+                                <ErrorMessage name="password" component="div" className="text-red-500 text-xs mt-1" />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">
+                                    Confirm Password
+                                </label>
+                                <Field name="confirmPassword" type="password" className="form-input border border-gray-300 rounded" />
+                                <ErrorMessage name="confirmPassword" component="div" className="text-red-500 text-xs mt-1" />
+                            </div>
+                            <div className="mb-4">
+                                <Field name="acceptTerms" type="checkbox" className="form-checkbox" />
+                                <label htmlFor="acceptTerms" className="ml-2 text-sm">
+                                    I have read and agree to the Terms
+                                </label>
+                                <ErrorMessage name="acceptTerms" component="div" className="text-red-500 text-xs mt-1" />
+                            </div>
+                            <button
+                                type="submit"
+                                    className="bg-color-tertiary hover:bg-rose-500 text-white px-3 py-1 rounded-full text-base cursor"
+                            >
+                            Sign Up
+                            </button>
+                           
+                            <p className="mt-2 text-sm">
+                                Already have an account?{' '}
+                                <span
+                                    className="text-color-tertiary cursor-pointer"
+                                    onClick={handleLoginLinkClick}
+                                >
+                                    Login
+                                </span>
+                            </p>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
