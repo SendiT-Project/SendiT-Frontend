38c61f99245ceb0dece8dbd1bc57ae6ae283d0c1
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-12-01 22:30:48 +0300

    GET all orders

--- src/App.js
+++ src/App.js
@@ -8,11 +8,33 @@ import Login from "./components/Login";
 import Contact from './components/Contact';
 import { useEffect, useState } from 'react';
 import { enqueueSnackbar } from 'notistack';
+import AdminOrders from './components/AdminOrders';
 import Tracker from './components/Tracker';
 
 function App() {
   const [user, setUser] = useState({})
   const [isLoggedIn, setIsLoggedIn] = useState({})
+  const [orders, setOrders] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] =useState(1)
+  const [ordersPerPage, setOrdersPerPage] = useState(5)
+
+
+  useEffect(() =>{
+    setLoading(true)
+    fetch('/orders', {credentials: "include"})
+    .then(r =>r.json())
+    .then(data =>{
+      setOrders(data)
+      setLoading(false)
+    })
+    .catch(error => {
+      console.log("Error fetching session:", error)
+    })
+  }, [])
+
+  console.log(orders)
+
 
   useEffect(() =>{
     fetch('/session',{credentials: "include"})
@@ -60,7 +82,8 @@ function App() {
         <Route path="/orders" element={<Orders/>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/contact" element={<Contact/>} />
-        <Route path="/tracking" element={<Tracker/>} />
+        <Route path="/tracker" element={<Tracker />} />
+        <Route path="/adminOrders" element={<AdminOrders orders={orders} loading={loading}/>} />
       </Routes>
       <Footer/>
     </Router>
